eb8626405b825b189e2df4bb8ab5c7b6f80aaf33
uyuiquoh <uyuiquoh@gmail.com>
2023-12-15 20:58:33 +0800

    查看购物车业务代码开发

--- sky-server/src/main/java/com/sky/service/ShoppingCartService.java
+++ sky-server/src/main/java/com/sky/service/ShoppingCartService.java
@@ -1,13 +1,23 @@
 package com.sky.service;
 
 import com.sky.dto.ShoppingCartDTO;
-import org.springframework.stereotype.Service;
+import com.sky.entity.ShoppingCart;
+
+import java.util.List;
 
 
 public interface ShoppingCartService {
     /**
      * 添加购物车
+     *
      * @param shoppingCartDTO
      */
     void addShoppingCart(ShoppingCartDTO shoppingCartDTO);
+
+    /**
+     * 查看购物车
+     *
+     * @return
+     */
+    List<ShoppingCart> showShoppingCart();
 }
